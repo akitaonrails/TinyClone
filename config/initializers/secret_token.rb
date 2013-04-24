b6d4b2e6bb54b117a0360ef8a3d42628c603af3d
AkitaOnRails <fabioakita@gmail.com>
2013-04-24 14:03:18 -0300

    adding sample secret token (use ENV though)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,9 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-secret = ENV['RAILS_SECRET_TOKEN'] || ""
-if secret.length < 30
-  raise "Secret token cannot be loaded"
-else
-  Tinyclone2::Application.config.secret_token = secret
+secret = ENV['RAILS_SECRET_TOKEN']
+if secret.nil? || secret.length < 30
+  # MUST CHANGE!!
+  secret = "7b02b842f8d882f929984c9ea2ef4ce3765b5794245f8ea4bfd9e8292fbd1c87ac2372a8508f9531a389964c8db88a0583dd45375b376f4bb00a888fc4df6cd6"
 end
+Tinyclone2::Application.config.secret_token = secret
